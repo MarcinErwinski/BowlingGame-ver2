71c6b11a5219be90fa190abdd0f4401a50e24efe
Erwin <marerw002@utp.edu.pl>
2017-04-06 13:29:25 +0200

    full usefull bowling

---
+++ src/main/java/com/mycompany/bowlinggame/BowlingGame.java
@@ -0,0 +1,50 @@
+package com.mycompany.bowlinggame;
+/**
+ *
+ * @author Erwin
+ */
+public class BowlingGame {
+
+    private int[] rolls = new int[21];
+    private int currentRoll = 0;
+
+    public void roll(int pins) {
+        rolls[currentRoll++] = pins;
+    }
+
+    public int getScore() {
+        int score = 0;
+        int rollIndex=0;
+        for(int frameIndex=0;frameIndex<10;frameIndex++){
+            if (isStrike(rollIndex)){
+                score+=getStrikeScore(rollIndex);
+                rollIndex++;
+            } else if (isSpare(rollIndex)){
+                score+=getSpareScore(rollIndex);
+                rollIndex+=2;
+            } else{
+                score+=getStandardScore(rollIndex);
+                rollIndex+=2;
+            }
+        }
+        return score;
+    }
+    private boolean isStrike(int rollIndex){
+        
+        return rolls[rollIndex]==10;
+    }
+    private boolean isSpare(int rollIndex){
+        return rolls[rollIndex]+rolls[rollIndex+1]==10;
+    }
+    private int getStrikeScore(int rollIndex){
+         return rolls[rollIndex]+rolls[rollIndex+1]+rolls[rollIndex+2]; 
+    }
+    private int getSpareScore(int rollIndex){
+         return rolls[rollIndex]+rolls[rollIndex+1]+rolls[rollIndex+2]; 
+    }
+    private int getStandardScore(int rollIndex){
+         return rolls[rollIndex]+rolls[rollIndex+1];
+    }
+    
+  
+}
